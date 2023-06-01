227c8cd7e878328d705d7221a13fd8e23ada6d6d
MalachiRutter <malachicoberley@gmail.com>
2023-06-01 11:15:28 -0700

    Added Icon and Stack navigation

--- App.js
+++ App.js
@@ -1,30 +1,53 @@
-import * as SQLite from "expo-sqlite";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { Image, StyleSheet, View } from "react-native";
 import Button from "./components/Button";
 
-export default function App() {
-  const db = SQLite.openDatabase("scores.db");
-  const [isLoading, setIsLoading] = useState(false);
+const Stack = createNativeStackNavigator();
+const trophyIcon = require("./assets/trophy.png");
 
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <Text> Loading... </Text>
-      </View>
-    );
-  }
+const App = () => {
+  // const db = SQLite.openDatabase("scores.db");
+  // const [isLoading, setIsLoading] = useState(false);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
+const HomeScreen = ({ navigation }) => {
+  const clicked = () => alert("You FUCKEWED YOUESELF a button.");
+  return (
+    <View style={styles.container}>
+      <Button label="Start A New Game" onPressFunction={clicked}></Button>
+      <Image source={trophyIcon} style={styles.image}></Image>
+      <Button label="Resume Game" onPressFunction={clicked}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const NewGameScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Button label="Start A New Game"></Button>
-      <Text>Score Keeping Baaaaby</Text>
+      <Image source={trophyIcon} style={styles.image}></Image>
       <Button label="Resume Game"></Button>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -33,4 +56,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  image: {
+    width: 320,
+    height: 320,
+    marginTop: 40,
+    marginBottom: 40,
+  },
 });
+
+export default App;
--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,11 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Button({ label }) {
+export default function Button({ onPressFunction, label }) {
   return (
     <View style={styles.buttonContainer}>
-      <Pressable
-        style={styles.button}
-        onPress={() => alert("You pressed a button.")}
-      >
+      <Pressable style={styles.button} onPress={onPressFunction}>
         <Text style={styles.buttonLabel}>{label}</Text>
-      </Pressable>
+      </Pressable> 
     </View>
   );
 }
@@ -30,6 +27,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
+    zIndex: 0,
   },
   buttonLabel: {
     color: "#fff",
